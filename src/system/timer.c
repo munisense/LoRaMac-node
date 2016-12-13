2e3f46c84c3db4f7bea63de457b706b51688af93
Andrea Bernasconi <andrea@munisense.com>
2016-12-13 16:00:28 +0100

    rewrite time handler

--- src/system/timer.c
+++ src/system/timer.c
@@ -1,415 +1,123 @@
-/*
- / _____)             _              | |
-( (____  _____ ____ _| |_ _____  ____| |__
- \____ \| ___ |    (_   _) ___ |/ ___)  _ \
- _____) ) ____| | | || |_| ____( (___| | | |
-(______/|_____)_|_|_| \__)_____)\____)_| |_|
-    (C)2013 Semtech
+#include "timer.h"
+#include "math.h"
 
-Description: Timer objects and scheduling management
-
-License: Revised BSD License, see LICENSE.TXT file include in the project
-
-Maintainer: Miguel Luis and Gregory Cristian
-*/
-#include "board.h"
-#include "rtc-board.h"
-
-
-/*!
- * This flag is used to make sure we have looped through the main several time to avoid race issues
- */
-volatile uint8_t HasLoopedThroughMain = 0;
+#define TICKS_IN_1_S 1024.0
 
 /*!
  * Timers list head pointer
  */
 static TimerEvent_t *TimerListHead = NULL;
 
-/*!
- * \brief Adds or replace the head timer of the list.
- *
- * \remark The list is automatically sorted. The list head always contains the
- *         next timer to expire.
- *
- * \param [IN]  obj Timer object to be become the new head
- * \param [IN]  remainingTime Remaining time of the previous head to be replaced
- */
-static void TimerInsertNewHeadTimer( TimerEvent_t *obj, uint32_t remainingTime );
+static TimerTime_t getMilliseconds();
+static void removeFromList(TimerEvent_t *obj);
+static void addToList(TimerEvent_t *obj);
+static void scheduleEvent(int32u milliseconds);
+static void printList();
 
-/*!
- * \brief Adds a timer to the list.
- *
- * \remark The list is automatically sorted. The list head always contains the
- *         next timer to expire.
- *
- * \param [IN]  obj Timer object to be added to the list
- * \param [IN]  remainingTime Remaining time of the running head after which the object may be added
- */
-static void TimerInsertTimer( TimerEvent_t *obj, uint32_t remainingTime );
-
-/*!
- * \brief Sets a timeout with the duration "timestamp"
- *
- * \param [IN] timestamp Delay duration
- */
-static void TimerSetTimeout( TimerEvent_t *obj );
-
-/*!
- * \brief Check if the Object to be added is not already in the list
- *
- * \param [IN] timestamp Delay duration
- * \retval true (the object is already in the list) or false
- */
-static bool TimerExists( TimerEvent_t *obj );
-
-/*!
- * \brief Read the timer value of the currently running timer
- *
- * \retval value current timer value
- */
-TimerTime_t TimerGetValue( void );
+EmberEventControl loraMacNodeTimingEventControl;
 
 void TimerInit( TimerEvent_t *obj, void ( *callback )( void ) )
 {
-    obj->Timestamp = 0;
+    obj->ExpiryTime = 0;
     obj->ReloadValue = 0;
-    obj->IsRunning = false;
     obj->Callback = callback;
     obj->Next = NULL;
 }
 
+void TimerSetValue( TimerEvent_t *obj, int32u value )
+{
+    removeFromList(obj);
+    obj->ReloadValue = value;
+//emberAfPrintln(0xFFFF, "%s: Set timer %x value to %X%X%X%X", __func__, obj, (obj->ReloadValue)>>24, (obj->ReloadValue)>>16, (obj->ReloadValue)>>8, obj->ReloadValue);
+}
+
 void TimerStart( TimerEvent_t *obj )
 {
-    uint32_t elapsedTime = 0;
-    uint32_t remainingTime = 0;
-
-    __disable_irq( );
-
-    if( ( obj == NULL ) || ( TimerExists( obj ) == true ) )
-    {
-        __enable_irq( );
-        return;
-    }
-
-    obj->Timestamp = obj->ReloadValue;
-    obj->IsRunning = false;
-
-    if( TimerListHead == NULL )
-    {
-        TimerInsertNewHeadTimer( obj, obj->Timestamp );
-    }
-    else
-    {
-        if( TimerListHead->IsRunning == true )
-        {
-            elapsedTime = TimerGetValue( );
-            if( elapsedTime > TimerListHead->Timestamp )
-            {
-                elapsedTime = TimerListHead->Timestamp; // security but should never occur
-            }
-            remainingTime = TimerListHead->Timestamp - elapsedTime;
-        }
-        else
-        {
-            remainingTime = TimerListHead->Timestamp;
-        }
-
-        if( obj->Timestamp < remainingTime )
-        {
-            TimerInsertNewHeadTimer( obj, remainingTime );
-        }
-        else
-        {
-             TimerInsertTimer( obj, remainingTime );
-        }
-    }
-    __enable_irq( );
-}
-
-static void TimerInsertTimer( TimerEvent_t *obj, uint32_t remainingTime )
-{
-    uint32_t aggregatedTimestamp = 0;      // hold the sum of timestamps
-    uint32_t aggregatedTimestampNext = 0;  // hold the sum of timestamps up to the next event
-
-    TimerEvent_t* prev = TimerListHead;
-    TimerEvent_t* cur = TimerListHead->Next;
-
-    if( cur == NULL )
-    { // obj comes just after the head
-        obj->Timestamp -= remainingTime;
-        prev->Next = obj;
-        obj->Next = NULL;
-    }
-    else
-    {
-        aggregatedTimestamp = remainingTime;
-        aggregatedTimestampNext = remainingTime + cur->Timestamp;
-
-        while( prev != NULL )
-        {
-            if( aggregatedTimestampNext > obj->Timestamp )
-            {
-                obj->Timestamp -= aggregatedTimestamp;
-                if( cur != NULL )
-                {
-                    cur->Timestamp -= obj->Timestamp;
-                }
-                prev->Next = obj;
-                obj->Next = cur;
-                break;
-            }
-            else
-            {
-                prev = cur;
-                cur = cur->Next;
-                if( cur == NULL )
-                { // obj comes at the end of the list
-                    aggregatedTimestamp = aggregatedTimestampNext;
-                    obj->Timestamp -= aggregatedTimestamp;
-                    prev->Next = obj;
-                    obj->Next = NULL;
-                    break;
-                }
-                else
-                {
-                    aggregatedTimestamp = aggregatedTimestampNext;
-                    aggregatedTimestampNext = aggregatedTimestampNext + cur->Timestamp;
-                }
-            }
-        }
-    }
-}
-
-static void TimerInsertNewHeadTimer( TimerEvent_t *obj, uint32_t remainingTime )
-{
-    TimerEvent_t* cur = TimerListHead;
-
-    if( cur != NULL )
-    {
-        cur->Timestamp = remainingTime - obj->Timestamp;
-        cur->IsRunning = false;
-    }
-
-    obj->Next = cur;
-    obj->IsRunning = true;
-    TimerListHead = obj;
-    TimerSetTimeout( TimerListHead );
-}
-
-void TimerIrqHandler( void )
-{
-    uint32_t elapsedTime = 0;
+    boolean scheduleNewEvent = FALSE;
     
-    // Early out when TimerListHead is null to prevent nullpointer
-    if ( TimerListHead == NULL ) 
-    {
-        return;
+    if (TimerListHead == NULL) {
+        scheduleNewEvent = TRUE;
     }
-
-    elapsedTime = TimerGetValue( );
     
-    if( elapsedTime >= TimerListHead->Timestamp )
-    {
-        TimerListHead->Timestamp = 0;
-    }
-    else
-    {
-        TimerListHead->Timestamp -= elapsedTime;
-    }
+    obj->ExpiryTime = getMilliseconds() + obj->ReloadValue;
+    addToList(obj);
+//emberAfPrintln(0xFFFF, "%s: Started timer %x. Will expire at time %X%X%X%X", __func__, obj, (obj->ExpiryTime)>>24, (obj->ExpiryTime)>>16, (obj->ExpiryTime)>>8, obj->ExpiryTime);
+//printList();
 
-    TimerListHead->IsRunning = false;
-
-    while( ( TimerListHead != NULL ) && ( TimerListHead->Timestamp == 0 ) )
-    {
-        TimerEvent_t* elapsedTimer = TimerListHead;
-        TimerListHead = TimerListHead->Next;
-
-        if( elapsedTimer->Callback != NULL )
-        {
-            elapsedTimer->Callback( );
-        }
-    }
-
-    // start the next TimerListHead if it exists
-    if( TimerListHead != NULL )
-    {
-        if( TimerListHead->IsRunning != true )
-        {
-            TimerListHead->IsRunning = true;
-            TimerSetTimeout( TimerListHead );
-        }
+    if (scheduleNewEvent) {
+        scheduleEvent(obj->ExpiryTime - getMilliseconds());
     }
 }
 
 void TimerStop( TimerEvent_t *obj )
 {
-    __disable_irq( );
-
-    uint32_t elapsedTime = 0;
-    uint32_t remainingTime = 0;
-
-    TimerEvent_t* prev = TimerListHead;
-    TimerEvent_t* cur = TimerListHead;
-
-    // List is empty or the Obj to stop does not exist
-    if( ( TimerListHead == NULL ) || ( obj == NULL ) )
-    {
-        __enable_irq( );
-        return;
-    }
-
-    if( TimerListHead == obj ) // Stop the Head
-    {
-        if( TimerListHead->IsRunning == true ) // The head is already running
-        {
-            elapsedTime = TimerGetValue( );
-            if( elapsedTime > obj->Timestamp )
-            {
-                elapsedTime = obj->Timestamp;
-            }
-
-            remainingTime = obj->Timestamp - elapsedTime;
-
-            if( TimerListHead->Next != NULL )
-            {
-                TimerListHead->IsRunning = false;
-                TimerListHead = TimerListHead->Next;
-                TimerListHead->Timestamp += remainingTime;
-                TimerListHead->IsRunning = true;
-                TimerSetTimeout( TimerListHead );
-            }
-            else
-            {
-                TimerListHead = NULL;
-            }
-        }
-        else // Stop the head before it is started
-        {
-            if( TimerListHead->Next != NULL )
-            {
-                remainingTime = obj->Timestamp;
-                TimerListHead = TimerListHead->Next;
-                TimerListHead->Timestamp += remainingTime;
-            }
-            else
-            {
-                TimerListHead = NULL;
-            }
-        }
-    }
-    else // Stop an object within the list
-    {
-        remainingTime = obj->Timestamp;
-
-        while( cur != NULL )
-        {
-            if( cur == obj )
-            {
-                if( cur->Next != NULL )
-                {
-                    cur = cur->Next;
-                    prev->Next = cur;
-                    cur->Timestamp += remainingTime;
-                }
-                else
-                {
-                    cur = NULL;
-                    prev->Next = cur;
-                }
-                break;
-            }
-            else
-            {
-                prev = cur;
-                cur = cur->Next;
-            }
-        }
-    }
-    __enable_irq( );
-}
-
-static bool TimerExists( TimerEvent_t *obj )
-{
-    TimerEvent_t* cur = TimerListHead;
-
-    while( cur != NULL )
-    {
-        if( cur == obj )
-        {
-            return true;
-        }
-        cur = cur->Next;
-    }
-    return false;
-}
-
-void TimerReset( TimerEvent_t *obj )
-{
-    TimerStop( obj );
-    TimerStart( obj );
-}
-
-void TimerSetValue( TimerEvent_t *obj, uint32_t value )
-{
-    TimerStop( obj );
-    obj->Timestamp = value;
-    obj->ReloadValue = value;
-}
-
-TimerTime_t TimerGetValue( void )
-{
-    return RtcGetElapsedAlarmTime( );
-}
-
-TimerTime_t TimerGetCurrentTime( void )
-{
-    return RtcGetTimerValue( );
+    removeFromList(obj);
+//emberAfPrintln(0xFFFF, "%s: Stopped timer %x", __func__, obj);
+//printList();
 }
 
 TimerTime_t TimerGetElapsedTime( TimerTime_t savedTime )
 {
-    return RtcComputeElapsedTime( savedTime );
-}
-
-TimerTime_t TimerGetFutureTime( TimerTime_t eventInFuture )
-{
-    return RtcComputeFutureEventTime( eventInFuture );
-}
-
-static void TimerSetTimeout( TimerEvent_t *obj )
-{
-    HasLoopedThroughMain = 0;
-    obj->Timestamp = RtcGetAdjustedTimeoutValue( obj->Timestamp ); 
-    RtcSetTimeout( obj->Timestamp );
-}
-
-void TimerLowPowerHandler( void )
-{
-    if( ( TimerListHead != NULL ) && ( TimerListHead->IsRunning == true ) )
+    // Needed at boot, cannot compute with 0 or elapsed time will be equal to current time
+    if( savedTime == 0 )
     {
-        if( HasLoopedThroughMain < 5 )
-        {
-            HasLoopedThroughMain++;
+        return 0;
+    }
+    
+    return (getMilliseconds() - savedTime);
+}
+
+TimerTime_t TimerGetCurrentTime( void )
+{
+    return getMilliseconds();
+}
+
+void TimerIrqHandler( void )
+{
+    TimerTime_t currentTime = getMilliseconds();
+    TimerTime_t timeToEvent;
+    TimerTime_t timeToNextEvent = 0xFFFFFFFF;
+    boolean scheduleNewEvent = FALSE;
+    
+    void ( *currentTimerCallback )( void );
+    TimerTime_t currentTimerExpiryTime;
+    TimerEvent_t* currentTimerPointer;
+    
+    TimerEvent_t* cur = TimerListHead;
+    
+    //scan the entire list
+    while (cur != NULL) {
+        //store the relevant information of the current timer and move to the next one
+        //(just in case the callback changes any property of the timer itself)
+        currentTimerCallback = cur->Callback;
+        currentTimerExpiryTime = cur->ExpiryTime;
+        currentTimerPointer = cur;
+        cur = cur->Next;
+        
+        if (currentTime >= currentTimerExpiryTime) {
+            //current timer has expired: time to perform its action
+            currentTimerCallback();
+            removeFromList(currentTimerPointer);
         }
-        else
-        {
-            HasLoopedThroughMain = 0;
-            if( GetBoardPowerSource( ) == BATTERY_POWER )
-            {
-                RtcEnterLowPowerStopMode( );
+        else {
+            //current timer has not yet expired: determine when the next timer event will occur
+            scheduleNewEvent = TRUE;
+            timeToEvent = currentTimerExpiryTime - currentTime;
+            if (timeToEvent < timeToNextEvent) {
+                timeToNextEvent = timeToEvent;
             }
         }
     }
+    
+    if (scheduleNewEvent) {
+        scheduleEvent(timeToNextEvent);
+    }
 }
-
-extern uint32_t RtcCalendarContext;
 
 void zeroTimer(TimerEvent_t *obj)
 {
-    obj->Timestamp = 0;
+    obj->ExpiryTime = 0;
     obj->ReloadValue = 0;
-    obj->IsRunning = false;
     obj->Callback = NULL;
     obj->Next = NULL;
 }
@@ -423,7 +131,9 @@ extern TimerEvent_t TxTimeoutTimer;
 extern TimerEvent_t RxTimeoutTimer;
 extern TimerEvent_t RxTimeoutSyncWord;
 void loraMacNodeInitTimers()
-{
+{    
+    emberEventControlSetInactive(loraMacNodeTimingEventControl);
+    
     //Zero all existing timers and clear timer list
     zeroTimer(&MacStateCheckTimer);
     zeroTimer(&TxDelayedTimer);
@@ -436,6 +146,59 @@ void loraMacNodeInitTimers()
     TimerListHead = NULL;
     
     //Zero all other state variables
-    HasLoopedThroughMain = 0;
-    RtcCalendarContext = 0;
+}
+
+static TimerTime_t getMilliseconds()
+{   
+    float ms = (float)halCommonGetInt32uMillisecondTick() * (float)1000.0 / (float)TICKS_IN_1_S;
+    return (TimerTime_t)roundf(ms);
+}
+
+static void removeFromList(TimerEvent_t *obj)
+{
+    TimerEvent_t **prev, **cur;
+
+    cur = &TimerListHead;
+    if (*cur == NULL) {
+        //List is empty: nothing to do
+        return;
+    }
+    
+    while (*cur != NULL) {
+        prev = cur;
+        cur = &((*cur)->Next);
+        if (*prev == obj) {
+            //found the item: remove it
+            *prev = (*prev)->Next;
+        }
+    }
+}
+
+static void addToList(TimerEvent_t *obj)
+{
+    obj->Next = TimerListHead;
+    TimerListHead = obj;
+}
+
+static void printList()
+{
+    TimerEvent_t *cur = TimerListHead;
+    
+    emberAfPrint(0xFFFF, "%s: Timer list =", __func__);
+    while (cur != NULL) {
+        emberAfPrint(0xFFFF, " %x", cur);
+        cur = cur->Next;
+    }
+    emberAfPrint(0xFFFF, "\n", __func__);
+}
+
+static void scheduleEvent(int32u milliseconds)
+{
+    emberAfEventControlSetDelay(&loraMacNodeTimingEventControl, (int32u)((float)milliseconds * 1.024));
+}
+
+void loraMacNodeTimingHandler()
+{
+    emberEventControlSetInactive(loraMacNodeTimingEventControl);
+    TimerIrqHandler();
 }
